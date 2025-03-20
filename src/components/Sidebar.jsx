a3c602e0a979542ae65c6e079b16d56b9e840553
deekshithm0 <deekshithm0@gmail.com>
2025-03-20 11:25:50 +0530

    first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='w-1/5 h-full rounded-2xl bg-blue-400'>Sidebar</div>
+  )
+}
+
+export default Sidebar
---
+++ src/components/StaticCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StaticCard = () => {
+  return (
+    <div>StaticCard</div>
+  )
+}
+
+export default StaticCard
